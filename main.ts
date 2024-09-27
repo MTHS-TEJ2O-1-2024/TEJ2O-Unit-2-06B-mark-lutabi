a60e4f5e3c09d675dca89a2c6786fad576ba893b
mark-lutabi <mark.lutabi@mths.ca>
2024-09-27 11:04:04 -0400

    changed my code

--- main.ts
+++ main.ts
@@ -1,8 +1,21 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Mark lutabi
+ * Created on: Sep 2024
+ * This program turns LED on and off with switch
 */
 
-basic.showString('Hello, World!')
+
+basic.clearScreen()
+pins.digitalWritePin(DigitalPin.P16, 0 )
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function () {
+pins.digitalWritePin(DigitalPin.P16,1)
+basic.showIcon(IconNames.Yes)
+})
+
+input.onButtonPressed(Button.B, function () {
+    pins.digitalWritePin(DigitalPin.P16, 0)
+    basic.showIcon(IconNames.No)
+})
